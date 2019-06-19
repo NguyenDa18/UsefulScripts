21b2eb4ba4ff99221759c696fee2eb0fd74932de
Danh Nguyen <danhnguyen267@gmail.com>
2019-06-19 13:59:26 -0700

    Update vscodeextensions.sh
    More extensions added to work with Clojure!

--- vscodeextensions.sh
+++ vscodeextensions.sh
@@ -1,14 +1,13 @@
 
 #!/bin/sh
 
-echo "Install VSCode extensions..."
+echo "Installing VSCode extensions..."
 code --install-extension 'DavidAnson.vscode-markdownlint'
 code --install-extension (dbaeumer.vscode-eslint)
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension eamodio.gitlens
 code --install-extension eg2.tslint
 code --install-extension esbenp.prettier-vscode
-code --install-extension johnpapa.Angular2
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension msjsdiag.debugger-for-chrome
@@ -16,3 +15,8 @@ code --install-extension octref.vetur
 code --install-extension pkosta2005.heroku-command
 code --install-extension ritwickdey.LiveServer
 code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension avli.clojure
+code --install-extension tonsky.clojure-warrio
+code --install-extension 2gua.rainbow-brackets
+code --install-extension stiansivertsen.visualclojure
+code --install-extension wasteamaccount.webtemplatestudio-dev-nightly
