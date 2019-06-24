d0b9e205e4af269988d25e5862a9dc4a40ab4372
Danh Nguyen <danhnguyen267@gmail.com>
2019-06-24 15:42:26 -0700

    Create important_cheatsheets.sh

---
+++ important_cheatsheets.sh
@@ -0,0 +1,2 @@
+curl -O https://www.atlassian.com/dam/jcr:8132028b-024f-4b6b-953e-e68fcce0c5fa/atlassian-git-cheatsheet.pdf
+curl -O https://github.com/ehmatthes/pcc/releases/download/v1.0.0/beginners_python_cheat_sheet_pcc_testing.pdf
