4022ba9a62479b5e0f71a0d4388efff7bbbc2b16
Danh Nguyen <danhnguyen267@gmail.com>
2019-06-25 08:45:31 -0700

    Update vscodeextensions.sh

--- vscodeextensions.sh
+++ vscodeextensions.sh
@@ -2,16 +2,14 @@
 #!/bin/sh
 
 echo "Installing VSCode extensions..."
-code --install-extension 'DavidAnson.vscode-markdownlint'
-code --install-extension (dbaeumer.vscode-eslint)
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
 code --install-extension dsznajder.es7-react-js-snippets
 code --install-extension eamodio.gitlens
-code --install-extension eg2.tslint
-code --install-extension esbenp.prettier-vscode
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension octref.vetur
 code --install-extension pkosta2005.heroku-command
 code --install-extension ritwickdey.LiveServer
 code --install-extension shd101wyy.markdown-preview-enhanced
@@ -20,3 +18,4 @@ code --install-extension tonsky.clojure-warrio
 code --install-extension 2gua.rainbow-brackets
 code --install-extension stiansivertsen.visualclojure
 code --install-extension wasteamaccount.webtemplatestudio-dev-nightly
+code --install-extension betterthantomorrow.calva
